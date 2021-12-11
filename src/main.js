682b31000830c9f32a6bbf511094a95e1161b258
berthahsu <271181958@qq.com>
2021-12-11 16:42:34 +0800

    封装DOM库小测试

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const testDiv = dom.find('#test>.red')[0]   // 获取对应的元素
+console.log(testDiv)
+
+dom.style(div, 'color', 'red')  // 设置 div.style.color
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
